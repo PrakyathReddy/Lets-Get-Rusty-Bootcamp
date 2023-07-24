80147c901298de9e78f43960d9a9fa2ca752965d
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-07-24 18:13:35 +0530

    variable type

---
+++ a_006/target/debug/a_006.d
@@ -0,0 +1,1 @@
+/Users/praky/Projects/Lets-Get-Rusty-Bootcamp/a_006/target/debug/a_006: /Users/praky/Projects/Lets-Get-Rusty-Bootcamp/a_006/src/main.rs
---
+++ a_006/target/debug/deps/a_006-c47039cba1985db0.d
@@ -0,0 +1,5 @@
+/Users/praky/Projects/Lets-Get-Rusty-Bootcamp/a_006/target/debug/deps/a_006-c47039cba1985db0: src/main.rs
+
+/Users/praky/Projects/Lets-Get-Rusty-Bootcamp/a_006/target/debug/deps/a_006-c47039cba1985db0.d: src/main.rs
+
+src/main.rs:
