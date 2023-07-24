26c95ab5ae888af8d007172ded96d23dbc0ae5e1
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-07-24 18:10:45 +0530

    boolean

---
+++ a_004/target/debug/a_004.d
@@ -0,0 +1,1 @@
+/Users/praky/Projects/Lets-Get-Rusty-Bootcamp/a_004/target/debug/a_004: /Users/praky/Projects/Lets-Get-Rusty-Bootcamp/a_004/src/main.rs
---
+++ a_004/target/debug/deps/a_004-359b7e4d6a5c0f62.d
@@ -0,0 +1,5 @@
+/Users/praky/Projects/Lets-Get-Rusty-Bootcamp/a_004/target/debug/deps/a_004-359b7e4d6a5c0f62: src/main.rs
+
+/Users/praky/Projects/Lets-Get-Rusty-Bootcamp/a_004/target/debug/deps/a_004-359b7e4d6a5c0f62.d: src/main.rs
+
+src/main.rs:
